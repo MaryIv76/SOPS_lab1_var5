07148a8b37230c8d4d1fb5d71cbd5722e2c841b5
MaryIv76 <ivanova_mary02@mail.ru>
2022-09-14 09:38:49 +0300

    Add start project

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace lab1_var5.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        public ApplicationContext()
+        {
+
+        }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
